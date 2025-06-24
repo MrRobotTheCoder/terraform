93ede74c28e3d98da417c0f7aba92bf3cf0edca3
Abinash Sethi <sethiabinash123@gmail.com>
2025-06-23 23:26:21 -0400

    create_monitoring_role set to false

--- 13.2-rds-db.tf
+++ 13.2-rds-db.tf
@@ -41,7 +41,7 @@ module "rdsdb" {
 
   performance_insights_enabled          = true
   performance_insights_retention_period = 7
-  create_monitoring_role                = true
+  create_monitoring_role                = false
   monitoring_interval                   = 60
 
   parameters = [
